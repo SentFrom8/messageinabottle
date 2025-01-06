5cf2f56344ff5af881f6eb562f2b5e5f6df6d714
SentFrom <sentfrom8@gmail.com>
2025-01-06 14:37:38 +0100

    fix: admin SDK credentials

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -4,18 +4,16 @@ import { getFirestore } from "firebase-admin/firestore";
 const apps = getApps();
 
 export const adminApp = () => {
-
     return apps.length ? apps[0] : initializeApp({
         credential: cert({
             projectId: process.env.GCP_PROJECT_ID,
             clientEmail: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
-            privateKey: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+            privateKey: process.env.GCP_PRIVATE_KEY,
         }),
     });
 };
 
 export const adminDb = () => {
-    console.log("adminApp ran");
     return getFirestore(adminApp());
 };
 
